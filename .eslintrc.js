d4b6976a08eb391cabdfffe2a12c60ab1f13343a
quyhp <quyhp@eplatform.vn>
2020-04-02 11:16:38 +0700

    update .eslintrc and add .vscode/settings.json

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,6 +9,12 @@ module.exports = {
   },
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
+    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+    "prettier/prettier": [
+      "error",
+      {
+        printWidth: 120
+      }
+    ]
   }
 };
